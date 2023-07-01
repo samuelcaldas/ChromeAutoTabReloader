1cdba1bbe8dee67b304df279244aa594c75acfdd
samuelcaldas <samuel.caldas@gmail.com>
2023-06-30 23:01:28 -0300

    update the interface with current timer

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     const intervalInput = document.getElementById('interval');
     const saveButton = document.getElementById('save');
-
-    saveButton.addEventListener('click', () => {
-        const interval = parseInt(intervalInput.value, 10);
-        chrome.runtime.sendMessage({
-            type: 'setInterval',
-            interval: interval
+  
+    // Retrieve the timer value for the current tab from Chrome storage
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      if (tabs.length > 0) {
+        const tabId = tabs[0].id;
+        chrome.storage.sync.get(tabId.toString(), (items) => {
+          const interval = items[tabId] || 0;
+          intervalInput.value = interval;
         });
-        window.close();
+      }
     });
-});
+  
+    saveButton.addEventListener('click', () => {
+      const interval = parseInt(intervalInput.value, 10);
+      chrome.runtime.sendMessage({
+        type: 'setInterval',
+        interval: interval
+      });
+      // Save the timer value for the current tab to Chrome storage
+      chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        if (tabs.length > 0) {
+          const tabId = tabs[0].id;
+          chrome.storage.sync.set({[tabId]: interval});
+        }
+      });
+      window.close();
+    });
+  });
+  
